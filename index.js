ecdac3bc55667cd2df52ba16ebf8c3237eea0645
Mitibus <36730438+Mitibus@users.noreply.github.com>
2018-08-30 13:18:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,68 +17,93 @@ bot.on('ready', function(){
 
 });
 
-bot.on('message', message => {
-    if(message.channel.id === "450703148890456094"){
-        if (message.author.id != "159985870458322944"){
-            if (verif(message)){
-                addRole(message);
-            } else {
-                console.log("Erreur lors de la verif de la présentation")
+bot.on('messageReactionAdd', (reaction, user) => {
+    console.log("Reaction detectee")
+
+    let androidRole = reaction.message.guild.roles.find('name', 'android');
+    let arduinoRole = reaction.message.guild.roles.find('name', 'arduino');
+    let htmlRole = reaction.message.guild.roles.find('name', 'html');
+    let javaRole = reaction.message.guild.roles.find('name', 'java');
+    let jsRole = reaction.message.guild.roles.find('name', 'js');
+    let phpRole = reaction.message.guild.roles.find('name', 'php');
+    let pythonRole = reaction.message.guild.roles.find('name', 'python');
+    let raspberryRole = reaction.message.guild.roles.find('name', 'raspberry');
+    let rubyRole = reaction.message.guild.roles.find('name', 'ruby');
+
+    if (reaction.message.channel.id === "450690990655930370") {
+        if(reaction.emoji.name === ":android:"){
+            if(user.roles.find('name', 'android')){
+                user.removeRole(androidRole);
+            }else{
+                user.addRole(androidRole)
+                console.log("J'ajoute un role")
+            }
+        }
+        if(reaction.emoji.name === ":arduino:"){
+            if(user.roles.find('name', 'arduino')){
+                user.removeRole(arduinoRole);
+            }else{
+                user.addRole(arduinoRole)
+                console.log("J'ajoute un role")
+            }
+        }
+        if(reaction.emoji.name === ":html:"){
+            if(user.roles.find('name', 'html')){
+                user.removeRole(htmlRole);
+            }else{
+                user.addRole(htmlRole)
+                console.log("J'ajoute un role")
+            }
+        }
+        if(reaction.emoji.name === ":java:"){
+            if(user.roles.find('name', 'java')){
+                user.removeRole(javaRole);
+            }else{
+                user.addRole(javaRole)
+                console.log("J'ajoute un role")
+            }
+        }
+        if(reaction.emoji.name === ":js:"){
+            if(user.roles.find('name', 'js')){
+                user.removeRole(jsRole);
+            }else{
+                user.addRole(jsRole)
+                console.log("J'ajoute un role")
+            }
+        }
+        if(reaction.emoji.name === ":php:"){
+            if(user.roles.find('name', 'php')){
+                user.removeRole(phpRole);
+            }else{
+                user.addRole(phpRole)
+                console.log("J'ajoute un role")
+            }
+        }
+        if(reaction.emoji.name === ":python:"){
+            if(user.roles.find('name', 'python')){
+                user.removeRole(pythonRole);
+            }else{
+                user.addRole(pythonRole)
+                console.log("J'ajoute un role")
+            }
+        }
+        if(reaction.emoji.name === ":raspberry:"){
+            if(user.roles.find('name', 'raspberry')){
+                user.removeRole(raspberryRole);
+            }else{
+                user.addRole(raspberryRole)
+                console.log("J'ajoute un role")
+            }
+        }
+        if(reaction.emoji.name === ":ruby:"){
+            if(user.roles.find('name', 'ruby')){
+                user.removeRole(rubyRole);
+            }else{
+                user.addRole(rubyRole)
+                console.log("J'ajoute un role")
             }
         }
     }
 })
-
-function random(min, max){
-    min = Math.ceil(1);
-    max = Math.floor(3);
-    randNum = Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-function verif(message) {
-    var motsFigurant = ['je', "j'ai", "suis", "dev", "dév", "passioné", "passion", "bonjour", "hello", "hola"];
-    
-    for (var i = 0; i < motsFigurant.length ; i++){
-        var regex = new RegExp(motsFigurant[i].replace(/(.)/g, "$1+"));
-        var analyse = regex.test(message);
-
-        if (analyse){
-            return true;
-        }
-    }
-}
-
-function addRole(message){
-    // On cherche le rôle sur le serveur
-    let nouveau = message.guild.roles.find('name', 'Nouveau')
-    let membres = message.guild.roles.find('name', 'Membres')
-
-    // On supprime le role nouveau
-    if(message.member.roles.find('name', 'Nouveau')){
-        message.member.removeRole(nouveau)
-        message.member.addRole(membres)
-
-        var help_embed = new Discord.RichEmbed()
-            .setColor('#01B0F0')
-            .addField("Une erreur à été détécté : ", "   ***Processus de vérification de présentation*** : Impossible d'atribuer le rôle")
-            .addField("-----", "   Merci de jeter un coup d'oeil à la présentation");
-
-        if (message.member.addRole(membres)){
-            random();
-            if (randNum === 1 ){
-                message.reply("Super, nous te souhaitons la bienvenue dans nôtre communauté et bonne chance pour tes projets futurs...")
-            }
-            if (randNum === 2){
-                message.reply("Nous te remercions d'avoir choisi notre communauté et nous espérons que tu nous aidera à la faire connaître et à la maintenir en fonctionnement :thumbsup::ok_hand:")
-            }
-            if(randNum === 3){
-                message.reply("Merci d'avoir choisi nôtre communauté :smile:")
-            }
-
-        }else {
-            bot.channels("450703148890456094").sendEmbed(help_embed);
-        }
-    }
-}
 
 bot.login(token);
