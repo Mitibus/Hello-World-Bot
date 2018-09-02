db41464d2aa043336c7d5a7543c27a01fcd7445b
Mitibus <36730438+Mitibus@users.noreply.github.com>
2018-09-02 12:45:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,101 +17,37 @@ bot.on('ready', function(){
 
 });
 
-bot.on('message', message => {
-  // If the message is "ping"
-  if (message.content === 'ping') {
-    // Send "pong" to the same channel
-    message.channel.send('pong');
-  }
-});
+bot.on('message', message => {  
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
 
-bot.on('messageReactionAdd', (reaction, user) => {
-    console.log("Reaction detectee")
+    let messageArray = message.content.split(' ');
+    let command = messageArray[0];
+    let args = messageArray.slice(1);  
+    
+  if (command === `${prefix}addrole`){
+        console.log(messageArray);
+        console.log(command);
+        console.log(args);
 
-    let androidRole = reaction.message.guild.roles.find('name', 'android');
-    let arduinoRole = reaction.message.guild.roles.find('name', 'arduino');
-    let htmlRole = reaction.message.guild.roles.find('name', 'html');
-    let javaRole = reaction.message.guild.roles.find('name', 'java');
-    let jsRole = reaction.message.guild.roles.find('name', 'js');
-    let phpRole = reaction.message.guild.roles.find('name', 'php');
-    let pythonRole = reaction.message.guild.roles.find('name', 'python');
-    let raspberryRole = reaction.message.guild.roles.find('name', 'raspberry');
-    let rubyRole = reaction.message.guild.roles.find('name', 'ruby');
-
-    if (reaction.message.channel.id === "450690990655930370") {
-        if(reaction.emoji.name === ":android:"){
-            if(user.roles.find('name', 'android')){
-                user.removeRole(androidRole);
+        if (args.length > 0) {
+            if (args.length < 2) {
+                    if (message.guild.roles.find('name', args[0])){
+                        message.member.addRole(message.guild.roles.find('name', args[0]));
+                        message.reply("Rôle ajouté avec succés !")
+                    }else {
+                        message.reply("Aucun role ne correspond a vôtre demande !")
+                    }
             }else{
-                user.addRole(androidRole)
-                console.log("J'ajoute un role")
+                message.reply("Trop d'arguments précisés !");
             }
-        }
-        if(reaction.emoji.name === ":arduino:"){
-            if(user.roles.find('name', 'arduino')){
-                user.removeRole(arduinoRole);
-            }else{
-                user.addRole(arduinoRole)
-                console.log("J'ajoute un role")
-            }
-        }
-        if(reaction.emoji.name === ":html:"){
-            if(user.roles.find('name', 'html')){
-                user.removeRole(htmlRole);
-            }else{
-                user.addRole(htmlRole)
-                console.log("J'ajoute un role")
-            }
-        }
-        if(reaction.emoji.name === ":java:"){
-            if(user.roles.find('name', 'java')){
-                user.removeRole(javaRole);
-            }else{
-                user.addRole(javaRole)
-                console.log("J'ajoute un role")
-            }
-        }
-        if(reaction.emoji.name === ":js:"){
-            if(user.roles.find('name', 'js')){
-                user.removeRole(jsRole);
-            }else{
-                user.addRole(jsRole)
-                console.log("J'ajoute un role")
-            }
-        }
-        if(reaction.emoji.name === ":php:"){
-            if(user.roles.find('name', 'php')){
-                user.removeRole(phpRole);
-            }else{
-                user.addRole(phpRole)
-                console.log("J'ajoute un role")
-            }
-        }
-        if(reaction.emoji.name === ":python:"){
-            if(user.roles.find('name', 'python')){
-                user.removeRole(pythonRole);
-            }else{
-                user.addRole(pythonRole)
-                console.log("J'ajoute un role")
-            }
-        }
-        if(reaction.emoji.name === ":raspberry:"){
-            if(user.roles.find('name', 'raspberry')){
-                user.removeRole(raspberryRole);
-            }else{
-                user.addRole(raspberryRole)
-                console.log("J'ajoute un role")
-            }
-        }
-        if(reaction.emoji.name === ":ruby:"){
-            if(user.roles.find('name', 'ruby')){
-                user.removeRole(rubyRole);
-            }else{
-                user.addRole(rubyRole)
-                console.log("J'ajoute un role")
-            }
+        }else {
+            message.reply("Aucun rôle n'a été précisé pour être ajouté !");
         }
     }
-})
+    
+});
+
+
 
 bot.login(token);
